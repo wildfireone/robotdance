61f664eabc0a598acbf17bc98a2f4bfb0c8a2713
Wildfireone <wildfireone@gmail.com>
2018-08-28 21:16:35 +0100

    testing motor

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get('/motorTime', function(req, res) {
 });
 
 app.get('/drive', function(req, res) {
-
+console.log(req.query.portL +":"+ getPort(req.query.portL) +":"+req.query.portR +":"+ getPort(req.query.portR))
   if (req.query.portL && getPort(req.query.portL) && req.query.portR && getPort(req.query.portR)) {
     var motorL = new ev3dev.Motor(getPort(req.query.portL));
     var motorR = new ev3dev.Motor(getPort(req.query.portR));
