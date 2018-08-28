13174b45d094ec55338255a91b87c950dc447284
Wildfireone <wildfireone@gmail.com>
2018-08-28 20:24:28 +0100

    testing motor

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ const app = express()
 const ev3dev = require('ev3dev-lang');
 
 app.get('/', function(req, res) {
-  var motor = new ev3dev.Motor(ev3dev.OUTPUT_A);
-  if(!motor.connected) {
-      console.error("No motor was found on port A. Please connect a tacho motor to port A and try again.");
-      process.exit(1);
-  }
-  motor.runForDistance(360 * 10, 500, motor.stopActionValues.brake);
+  var motor = new ev3dev.Motor(ev3dev.OUTPUT_C);
+  if (!motor.connected)
+    console.log("No motor could be found. Are you sure that one is connected?");
+
+  console.log(' Port: ' + motor.address);
+  console.log(' Driver: ' + motor.driverName);
+  console.log(' Available commands: ' + motor.commands);
+
+  console.log('Sending motor command...');
+
+  motor.rampUpSp = 100;
+  motor.rampDownSp = 100;
+  motor.runForTime(1000, motor.maxSpeed / 2, motor.stopActionValues.brake);
+
+  do {
+    console.log("Motor speed: " + motor.speed);
+
+    { //Hack to sleep for time
+      //    SHOULD NOT BE USED IN PRODUCTION CODE
+      var start = new Date().getTime();
+      while (new Date().getTime() < start + 80) {;
+      }
+    }
+  } while (motor.speed > 10);
   res.send('Hello World!')
 });
 
