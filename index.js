0c36b0fc175145ad475b496d39f2d51664c82138
Wildfireone <wildfireone@gmail.com>
2018-08-28 21:02:29 +0100

    testing motor

--- index.js
+++ index.js
@@ -6,48 +6,61 @@ const ev3dev = require('ev3dev-lang');
 //?time=value_in_milliseconds&port=outputPort
 app.get('/motorTime', function(req, res) {
 
-if(req.query.port || getPort(req.query.port)){
-  var motor = new ev3dev.Motor(getPort(req.query.port));
-  var motortime = 1000;
-  if(req.query.time){motortime = parseInt(req.query.time)}
-  if (!motor.connected)
-    console.log("No motor could be found. Are you sure that one is connected?");
+  if (req.query.port || getPort(req.query.port)) {
+    var motor = new ev3dev.Motor(getPort(req.query.port));
+    var motortime = 1000;
+    if (req.query.time) {
+      motortime = parseInt(req.query.time)
+    }
+    if (motor.connected) {
 
-  console.log(' Port: ' + motor.address);
-  console.log(' Driver: ' + motor.driverName);
-  console.log(' Available commands: ' + motor.commands);
 
-  console.log('Sending motor command...');
+      console.log(' Port: ' + motor.address);
+      console.log(' Driver: ' + motor.driverName);
+      console.log(' Available commands: ' + motor.commands);
 
-  motor.rampUpSp = 100;
-  motor.rampDownSp = 100;
-  motor.runForTime(motorTime, motor.maxSpeed / 2, motor.stopActionValues.brake);
+      console.log('Sending motor command...');
 
-  res.send('Completed')
-}
-else{
-  res.send('mo port supplied')
-}
+      motor.rampUpSp = 100;
+      motor.rampDownSp = 100;
+      motor.runForTime(motorTime, motor.maxSpeed / 2, motor.stopActionValues.brake);
+
+      res.send('Completed')
+    } else {
+      console.log("No motor could be found. Are you sure that one is connected?");
+      res.send('no motor on that port')
+    }
+  } else {
+    res.send('no port supplied')
+  }
 });
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!')
 })
 
-var getPort = function(portID){
-  switch(portID){
-    case "A": return ev3dev.OUTPUT_A;
-    case "B": return ev3dev.OUTPUT_B;
-    case "C": return ev3dev.OUTPUT_C;
-    case "D": return ev3dev.OUTPUT_D;
-    default: return null;
+var getPort = function(portID) {
+  switch (portID) {
+    case "A":
+      return ev3dev.OUTPUT_A;
+    case "B":
+      return ev3dev.OUTPUT_B;
+    case "C":
+      return ev3dev.OUTPUT_C;
+    case "D":
+      return ev3dev.OUTPUT_D;
+    default:
+      return null;
   }
 }
 
-var getStop = function(stopCommand){
-  switch(stopCommand){
-    case "break": return motor.stopActionValues.break;
-    case "coast": return motor.stopActionValues.coast;
-    case "hold": return motor.stopActionValues.hold;
+var getStop = function(stopCommand) {
+  switch (stopCommand) {
+    case "break":
+      return motor.stopActionValues.break;
+    case "coast":
+      return motor.stopActionValues.coast;
+    case "hold":
+      return motor.stopActionValues.hold;
   }
 }
