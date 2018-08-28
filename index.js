79555dca3edd15589997a94a94fd9e0076209c4b
Wildfireone <wildfireone@gmail.com>
2018-08-28 20:54:37 +0100

    testing motor

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const ev3dev = require('ev3dev-lang');
 
-app.get('/', function(req, res) {
-  var motor = new ev3dev.Motor(ev3dev.OUTPUT_C);
+
+//?time=value_in_milliseconds&port=outputPort
+app.get('/motorTime', function(req, res) {
+
+if(req.query.port || getPort(req.query.port)){
+  var motor = new ev3dev.Motor(getPort(req.query.port));
+  var motortime = 1000;
+  if(req.query.time){motortime = parseInt(req.query.time)}
   if (!motor.connected)
     console.log("No motor could be found. Are you sure that one is connected?");
 
@@ -15,21 +21,33 @@ app.get('/', function(req, res) {
 
   motor.rampUpSp = 100;
   motor.rampDownSp = 100;
-  motor.runForTime(1000, motor.maxSpeed / 2, motor.stopActionValues.brake);
+  motor.runForTime(motorTime, motor.maxSpeed / 2, motor.stopActionValues.brake);
 
-  do {
-    console.log("Motor speed: " + motor.speed);
-
-    { //Hack to sleep for time
-      //    SHOULD NOT BE USED IN PRODUCTION CODE
-      var start = new Date().getTime();
-      while (new Date().getTime() < start + 80) {;
-      }
-    }
-  } while (motor.speed > 10);
-  res.send('Hello World!')
+  res.send('Completed')
+}
+else{
+  res.send('mo port supplied')
+}
 });
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!')
 })
+
+var getPort = function(portID){
+  switch(portID){
+    case A: return ev3dev.OUTPUT_A;
+    case B: return ev3dev.OUTPUT_B;
+    case C: return ev3dev.OUTPUT_C;
+    case D: return ev3dev.OUTPUT_D;
+    default: return null;
+  }
+}
+
+var getStop = function(stopCommand){
+  switch(stopCommand){
+    case "break": return motor.stopActionValues.break;
+    case "coast": return motor.stopActionValues.coast;
+    case "hold": return motor.stopActionValues.hold;
+  }
+}
