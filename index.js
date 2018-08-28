7167bb96bc9776845357654dcba63fa9344c75d3
Wildfireone <wildfireone@gmail.com>
2018-08-28 21:12:37 +0100

    testing motor

--- index.js
+++ index.js
@@ -35,6 +35,39 @@ app.get('/motorTime', function(req, res) {
   }
 });
 
+app.get('/drive', function(req, res) {
+
+  if (req.query.portL && getPort(req.query.portL) && req.query.portR && getPort(req.query.portR)) {
+    var motorL = new ev3dev.Motor(getPort(req.query.portL));
+    var motorR = new ev3dev.Motor(getPort(req.query.portR));
+    var motortime = 1000;
+    if (req.query.time) {
+      motortime = parseInt(req.query.time)
+    }
+    if (motor.connected) {
+
+
+      console.log(' Port: ' + motor.address);
+      console.log(' Driver: ' + motor.driverName);
+      console.log(' Available commands: ' + motor.commands);
+
+      console.log('Sending motor command...');
+
+      motor.rampUpSp = 100;
+      motor.rampDownSp = 100;
+      motorL.runForTime(motortime, motor.maxSpeed / 2, motor.stopActionValues.brake);
+      motorR.runForTime(motortime, motor.maxSpeed / 2, motor.stopActionValues.brake);
+
+      res.send('Completed')
+    } else {
+      console.log("No motor could be found. Are you sure that one is connected?");
+      res.send('no motor on that port')
+    }
+  } else {
+    res.send('no port supplied')
+  }
+});
+
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!')
 })
